9df1c58dbf2b8878971cb8264723ec66e7b3f4a4
RoNaranjo02 <130687333+RoNaranjo02@users.noreply.github.com>
2023-06-07 23:39:28 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+from claseManejador import manejadorVehiculos
+from menu import menuOpciones
+
+
+if __name__ == '__main__':
+
+    manejador = manejadorVehiculos()
+    manejador.cargarLista()
+    menu = menuOpciones()
+    menu.opciones(manejador)
